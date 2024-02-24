386f2838ce1ac543e097eaad333594be1c1195f2
DevArt_Govind <devart.govind352@gmail.com>
2024-02-24 18:13:59 +0530

    Update contact.component.ts

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -5,27 +5,21 @@ import { ScrollSmoother } from 'gsap-trial/ScrollSmoother';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
-
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef) {}
 
   ngOnInit(): void {
-
-
     gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-    const text = document.querySelector(".get-touch");
+    const text = document.querySelector('.get-touch');
 
     gsap.to(text, {
       duration: 4,
       rotation: 360,
       repeat: -1,
-      ease: "none"
+      ease: 'none',
     });
-
-
   }
 }
-
