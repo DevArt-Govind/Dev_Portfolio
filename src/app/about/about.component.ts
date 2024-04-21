852514542522185583ec575886048e76b9dee17a
DevArt_Govind <devart.govind352@gmail.com>
2024-04-21 22:13:28 +0530

    new-change-1

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,51 +3,51 @@ import { gsap } from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 import { AnimationsService } from 'src/services/animations.service';
 
-
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements AfterViewInit {
-
-  constructor(private el: ElementRef,
-    private animationService: AnimationsService) { }
+  constructor(
+    private el: ElementRef,
+    private animationService: AnimationsService
+  ) {}
 
   ngAfterViewInit(): void {
-
     gsap.registerPlugin(ScrollTrigger);
 
+    let container = this.el.nativeElement.querySelector(
+      '.about-heading-wrapper'
+    ) as HTMLElement;
+    let sectionHeading = container.querySelectorAll('.about-heading');
 
-    let container = this.el.nativeElement.querySelector(".about-heading-wrapper") as HTMLElement;
-    let sectionHeading = container.querySelectorAll(".about-heading");
-
-    let serviceContainer = this.el.nativeElement.querySelector(".services-heading-wrapper") as HTMLElement;
-    let servicesHeading = container.querySelectorAll(".services-heading");
+    let serviceContainer = this.el.nativeElement.querySelector(
+      '.services-heading-wrapper'
+    ) as HTMLElement;
+    let servicesHeading = container.querySelectorAll('.services-heading');
 
     this.animationService.animateHeading(container, sectionHeading);
     this.animationService.animateHeading(serviceContainer, servicesHeading);
 
-
-
     // text reveal on scroll effect
     const textElements = gsap.utils.toArray('.text');
 
-    textElements.forEach((text: any) => {
-      gsap.to(text, {
-        backgroundSize: '100%',
-        ease: 'none',
-        scrollTrigger: {
-          trigger: text,
-          start: 'center 70%',
-          end: 'center 20%',
-          scrub: true,
-          onUpdate: (self) => { // Update the opacity based on the scroll position
-            text.style.opacity = self.progress;
-          }
-        },
-      });
-    });
+    // textElements.forEach((text: any) => {
+    //   gsap.to(text, {
+    //     backgroundSize: '100%',
+    //     ease: 'none',
+    //     scrollTrigger: {
+    //       trigger: text,
+    //       start: 'center 70%',
+    //       end: 'center 20%',
+    //       scrub: true,
+    //       onUpdate: (self) => { // Update the opacity based on the scroll position
+    //         text.style.opacity = self.progress;
+    //       }
+    //     },
+    //   });
+    // });
 
     // para scale effect
     const allTextPara = gsap.utils.toArray('.about-para-text p');
@@ -60,20 +60,21 @@ export class AboutComponent implements AfterViewInit {
           start: 'top 80%', // Start the animation when the top of the text is 80% in view
           end: 'bottom 20%', // End the animation when the bottom of the text is 20% in view
           scrub: true, // Enable scrubbing for a smoother effect
-          onUpdate: (self) => { // Update the opacity based on the scroll position
+          onUpdate: (self) => {
+            // Update the opacity based on the scroll position
             text.style.opacity = self.progress;
 
             const scaleValue = 0.1 + self.progress;
             text.style.transform = `scale(${Math.min(scaleValue, 1)})`;
-          }
-        }
+          },
+        },
       });
     });
 
     // section slide reveal effect
 
-    const slices = this.el.nativeElement.querySelectorAll(".uncover_slice");
-    const images = this.el.nativeElement.querySelectorAll(".myimg");
+    const slices = this.el.nativeElement.querySelectorAll('.uncover_slice');
+    const images = this.el.nativeElement.querySelectorAll('.myimg');
 
     // Convert NodeList to Array
     const sliceArray = Array.from(slices);
@@ -86,17 +87,23 @@ export class AboutComponent implements AfterViewInit {
     tl.addLabel('start');
 
     // Add animations to the timeline
-    tl.to(sliceArray, 1, {
-      height: 0,
-      ease: 'power4.InOut',
-      stagger: { amount: 0.33 }
-    }, 'start')
-      .to(imageArray, 1.2, {
+    tl.to(
+      sliceArray,
+      1,
+      {
+        height: 0,
+        ease: 'power4.InOut',
+        stagger: { amount: 0.33 },
+      },
+      'start'
+    ).to(
+      imageArray,
+      1.2,
+      {
         scale: 1.3,
         ease: 'power4.InOut',
-      }, 'start');
-
-
+      },
+      'start'
+    );
   }
-
 }
